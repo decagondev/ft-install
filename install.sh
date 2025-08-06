79b62b552a2d63044c3b70ad0ee16a554fcd7adc
Tom Tarpey <tom.tarpey@lambdaschool.com>
2025-08-06 15:17:44 +0100

    Update install.sh
    Updated to Focus Tracker Beta 2

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@
 set -e
 
 # Define variables
-BINARY_URL="https://github.com/decagondev/ft-install/raw/refs/heads/main/b1/FocusTrackerB1"
+BINARY_URL="https://github.com/decagondev/ft-install/raw/refs/heads/main/b2/FocusTrackerB2"
 INSTALL_PATH="/usr/local/bin/focustracker"
 TEMP_FILE="/tmp/focustracker"
 
