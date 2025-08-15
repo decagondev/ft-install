1d050bb8e3f6e9180218a4bb1bdd89e3f33a8dee
Tom Tarpey <tom.tarpey@lambdaschool.com>
2025-08-15 04:27:52 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@
 set -e
 
 # Define variables
-BINARY_URL="https://github.com/decagondev/ft-install/raw/refs/heads/main/b2/FocusTrackerB2"
+BINARY_URL="https://github.com/decagondev/ft-install/raw/refs/heads/main/rc1/FocusTrackerRC1"
 INSTALL_PATH="/usr/local/bin/focustracker"
 TEMP_FILE="/tmp/focustracker"
 
